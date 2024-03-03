262cdeadc8724ec0a840195f2f8ab0061f0bc0cd
ductan2 <ndt11102003@gmail.com>
2024-03-03 09:52:30 +0700

    config: file jest

---
+++ jest.config.ts
@@ -0,0 +1,38 @@
+   import { Config } from "@jest/types";
+
+   const config: Config.InitialOptions = {
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      verbose: true, // show test results
+      coverageDirectory: 'coverage',
+      collectCoverage: true,
+      testPathIgnorePatterns: ['/node_modules/'],
+      transform: {
+         '^.+\\.ts?$': 'ts-jest'
+      },
+      testMatch: ['<rootDir>/src/**/test/*.ts'],
+      collectCoverageFrom: ['src/**/*.ts', '!src/**/test/*.ts?(x)', '!**/node_modules/**'], 
+      coverageThreshold: {
+         global: {
+            branches: 1,
+            functions: 1,
+            lines: 1,
+            statements: 1
+         }
+      },
+      coverageReporters: ['text-summary', 'lcov'],
+      moduleNameMapper: {
+         "@controllers/(.*)": ["<rootDir>src/controllers/$1"],
+         "@models/(.*)": ["<rootDir>src/models/$1"],
+         "@services/(.*)": ["<rootDir>src/services/$1"],
+         "@utils/(.*)": ["<rootDir>src/utils/$1"],
+         "@middlewares/(.*)": ["<rootDir>src/middlewares/$1"],
+         "@config/(.*)": ["<rootDir>src/configs/$1"],
+         "@interfaces/(.*)": ["<rootDir>src/interfaces/$1"],
+         "@root/(.*)": ["<rootDir>src/$1"],
+         "@routes/(.*)": ["<rootDir>src/routes/$1"],
+         "@queues/(.*)": ["<rootDir>src/queues/$1"],
+      }
+   };
+
+   export default config;
